566632e14f7cc9d164095f7165ffc3e560566085
kingsonseang <kingsonseang@gmail.com>
2025-05-17 04:46:55 +0100

    Enhance Astro project setup with React integration, Tailwind CSS configuration, and updated dependencies. Removed Welcome component and adjusted layout structure. Updated global styles and favicon for improved design consistency.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
+  integrations: [react()],
+  vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
   },
 });
